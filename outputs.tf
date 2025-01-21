5d3fc3609a44c66af048a5b926c8cbbd1fe09883
Maros Kukan <13705278+maroskukan@users.noreply.github.com>
2025-01-21 10:50:28 +0100

    fix outputs name

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "qa_ip" {
     value = module.qa.public_ip
 }
 
-output "qa_ip" {
+output "staging_ip" {
     value = module.staging.public_ip
 }
 
-output "qa_ip" {
+output "prod_ip" {
     value = module.prod.public_ip
 }
